c562ea7aa4d3c83e39b4f1431f8e5d16912aa39b
AlejandroLeyvaa <64874435+AlejandroLeyvaa@users.noreply.github.com>
2020-08-08 14:09:30 -0600

    configuración creada

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+//   devServer: {
+
+//   },
+  resolve: {
+    extensions: [".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.jpg|png|gif|woff|eot|ttf|svg|mp4|mp3$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 90000,
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "./src/styles/styles.css", to: "" }],
+    }),
+  ],
+};
